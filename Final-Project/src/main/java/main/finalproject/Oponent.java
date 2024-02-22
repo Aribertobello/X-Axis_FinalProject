acb0c61c4c84564f0dfe1989ee023a13edd86a44
Aribertobello <123769648+Aribertobello@users.noreply.github.com>
2024-02-22 09:14:41 -0500

    Delete Final-Project directory

--- Final-Project/src/main/java/main/finalproject/Oponent.java
+++
@@ -1,6 +0,0 @@
-package main.finalproject;
-
-import javafx.scene.shape.Rectangle;
-
-public class Oponent extends Character {
-}
--- Final-Project/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module main.finalproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens main.finalproject to javafx.fxml;
-    exports main.finalproject;
-}
