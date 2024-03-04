cecdc6703200891fbba420f7ad4c3f55aee9d7f9
Aribertobello <123769648+Aribertobello@users.noreply.github.com>
2024-03-03 22:40:25 -0500

    new stuff

--- BulletBattleGround/src/main/java/com/example/bulletbattleground/game/Projectile.java
+++ BulletBattleGround/src/main/java/com/example/bulletbattleground/game/Projectile.java
@@ -3,11 +3,13 @@ package com.example.bulletbattleground.game;
 import com.example.bulletbattleground.utility.Bounds;
 import com.example.bulletbattleground.utility.Coordinate;
 import com.example.bulletbattleground.utility.MovingBody;
+import com.example.bulletbattleground.utility.Vector;
 
 public class Projectile extends MovingBody {
     protected Coordinate coordinate;
     protected int damage;
     protected Bounds bounds;
+    protected Vector lift;
     protected void move(){}
     protected void release(){}
 
