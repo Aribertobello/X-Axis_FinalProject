9699d5ee13c4696ff53ed72f7733a548e22d90cd
2246982 <123512815+2246982@users.noreply.github.com>
2024-03-17 08:53:39 -0400

    Bullet rotation + graphics

--- BulletBattleGround/src/main/java/com/example/bulletbattleground/gameObjects/projectiles/Bullet.java
+++ BulletBattleGround/src/main/java/com/example/bulletbattleground/gameObjects/projectiles/Bullet.java
@@ -4,8 +4,13 @@ import com.example.bulletbattleground.game.Projectile;
 import com.example.bulletbattleground.utility.Coordinate;
 import com.example.bulletbattleground.utility.HitBox;
 import com.example.bulletbattleground.utility.Vector;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class Bullet extends Projectile {
 
@@ -15,11 +20,23 @@ public class Bullet extends Projectile {
     }
 
     public Bullet(){
-        this.getChildren().add(new Circle(2, Color.DARKGRAY));
+        Circle small_bullet = new Circle(10);
+        small_bullet.setRotate(90);
+        Image smallBulletImg = new Image("file:smallBullet.png");
+        small_bullet.setFill(new ImagePattern(smallBulletImg));
+        this.getChildren().add(small_bullet);
         this.damage = 3;
         this.lift = new Vector(0,-2.0);
         this.forces.add(lift);
         this.mass = 0.5;
+
+
+        Timeline bulletAnimation = new Timeline(new KeyFrame(Duration.millis(1), e
+                -> {
+            small_bullet.setRotate(velocity().angle());
+        }));
+        bulletAnimation.setCycleCount(Timeline.INDEFINITE);
+        bulletAnimation.play();
     }
     @Override
     public void move(double time) {
