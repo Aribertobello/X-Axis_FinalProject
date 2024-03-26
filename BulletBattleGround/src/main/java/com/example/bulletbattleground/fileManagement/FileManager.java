2bf9a221365c3cb556d910f1720b33718a5350d2
2246982 <123512815+2246982@users.noreply.github.com>
2024-03-26 11:36:50 -0400

    Matthew update
    Images sorta work

--- BulletBattleGround/src/main/java/com/example/bulletbattleground/fileManagement/FileManager.java
+++ BulletBattleGround/src/main/java/com/example/bulletbattleground/fileManagement/FileManager.java
@@ -60,6 +60,7 @@ public class FileManager {
     public static Mapp defaultMapPve() {
         Mapp map = new Mapp("space");
         //map.addObstacle(new SpaceShip(0, 200, 500));
+        map.addFighter(new Ally(200, 600, 1));
         map.addObstacle(new SpaceShip(20, 900, 600));
         map.addObstacle(new SpaceShip(-30, 1300, 800));
         map.addObstacle(new SpaceShip(25, 400, 100));
