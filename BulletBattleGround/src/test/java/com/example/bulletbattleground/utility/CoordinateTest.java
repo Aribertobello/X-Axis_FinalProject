bd2e1a3c38c5b6456e9ef82f181b7fdc86d5c8c8
Aribertobello <123769648+Aribertobello@users.noreply.github.com>
2024-03-25 20:13:58 -0400

    tests

--- BulletBattleGround/src/test/java/com/example/bulletbattleground/utility/CoordinateTest.java
+++ BulletBattleGround/src/test/java/com/example/bulletbattleground/utility/CoordinateTest.java
@@ -8,10 +8,20 @@ class CoordinateTest {
 
     @Test
     void distance() {
+        Coordinate coord1 = new Coordinate(0,0);
+        Coordinate coord2 = new Coordinate(-9,0);
+        double expectedResult = 9;
+        double result = coord1.distance(coord2);
+        assertEquals(expectedResult, result, 0.0001);
     }
 
     @Test
     void distanceVector() {
+        Coordinate coord1 = new Coordinate(0,0);
+        Coordinate coord2 = new Coordinate(-9,0);
+        Vector expectedResult = new Vector(-9,0);
+        Vector result = coord1.distanceVector(coord2);
+        assertEquals(expectedResult, result);
     }
 
     @Test
--- BulletBattleGround/src/test/java/com/example/bulletbattleground/utility/VectorTest.java
+++ BulletBattleGround/src/test/java/com/example/bulletbattleground/utility/VectorTest.java
@@ -8,30 +8,73 @@ class VectorTest {
 
     @Test
     void magnitude() {
+        Vector vector1 = new Vector(-3,4);
+        double expectedResult = 5;
+        double result = vector1.magnitude();
+        assertEquals(expectedResult, result, 0.0001);
     }
 
     @Test
     void unitVector() {
+        Vector vector1 = new Vector(-20,2);
+        Vector vector2 = new Vector(-9,0);
     }
 
     @Test
     void normal() {
+        Vector vector1 = new Vector(-5,9000);
+        Vector vector2 = new Vector(-9,0);
     }
 
     @Test
-    void dotProduct() {
+    void dotProduct1() {
+        Vector vector1 = new Vector(-5,9000);
+        Vector vector2 = new Vector(-9,0);
+
+        double expectedResult = 45;
+        double result = Vector.dotProduct(vector1,vector2);
+        assertEquals(expectedResult, result, 0.0001);
+    }
+    @Test
+    void dotProduct2() {
+        Vector vector1 = new Vector(1,1);
+        Vector vector2 = new Vector(1,1);
+
+        double expectedResult = 2;
+        double result = Vector.dotProduct(vector1,vector2);
+        assertEquals(expectedResult, result, 0.0001);
     }
 
     @Test
-    void crossProduct() {
+    void dotProduct3() {
+        Vector vector1 = new Vector(12,0);
+        Vector vector2 = new Vector(12,0);
+
+        double expectedResult = 144;
+        double result = Vector.dotProduct(vector1,vector2);
+        assertEquals(expectedResult, result, 0.0001);
     }
+    @Test
+    void dotProduct4() {
+        Vector vector1 = new Vector(0,12);
+        Vector vector2 = new Vector(12,0);
+
+        double expectedResult = 0;
+        double result = Vector.dotProduct(vector1,vector2);
+        assertEquals(expectedResult, result, 0.0001);
+    }
+
 
     @Test
     void vectorSum() {
+        Vector vector1 = new Vector(-5,9000);
+        Vector vector2 = new Vector(-9,0);
     }
 
     @Test
     void angle() {
+        Vector vector1 = new Vector(-5,5);
+
     }
 
     @Test
