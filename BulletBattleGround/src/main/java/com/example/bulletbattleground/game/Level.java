8126f1f1a89ae66092877d4d4cf56cbe7e9f6b5f
Aribertobello <123769648+Aribertobello@users.noreply.github.com>
2024-04-01 18:23:30 -0400

    new fxml structure

--- BulletBattleGround/src/main/java/com/example/bulletbattleground/game/Level.java
+++ BulletBattleGround/src/main/java/com/example/bulletbattleground/game/Level.java
@@ -1,12 +1,17 @@
 package com.example.bulletbattleground.game;
 
+import com.example.bulletbattleground.BattleGround;
 import com.example.bulletbattleground.gameObjects.Loot.Loot;
 import com.example.bulletbattleground.gameObjects.fighters.Ally;
 import com.example.bulletbattleground.utility.Coordinate;
-import javafx.geometry.Pos;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Line;
@@ -15,7 +20,47 @@ import javafx.stage.Stage;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.IOException;
+
 public class Level extends AnchorPane {
+
+    @FXML
+    @Getter
+    @Setter
+    private Pane headsUpDisplay;
+
+    @FXML
+    private AnchorPane container;
+
+    @FXML
+    private Label activeProjectileLabel;
+
+    @FXML
+    private Label angleLabel;
+
+    @FXML
+    private Label KELabel;
+
+    @FXML
+    private Label blankLabel;
+
+    @FXML
+    private ProgressBar healthProgressbar;
+
+    @FXML
+    private Label healthLabel;
+
+    @FXML
+    private MenuBar topMenu;
+
+    @FXML
+    private Menu exitButton;
+
+    @FXML
+    private Menu settingsButton;
+
+    @FXML
+    private Menu pauseButton;
 
     @Getter
     @Setter
@@ -23,7 +68,7 @@ public class Level extends AnchorPane {
 
     protected Mapp map;
 
-    protected HBox headsUpDisplay = new HBox(new Label("HUD"));
+
     // Label angleLabel = new Label("Angle: " + AngleCalculation);
 
     protected Line trajectoryLine = new Line();//TODO
@@ -36,48 +81,41 @@ public class Level extends AnchorPane {
 
     static int screenWidth = (int) Screen.getPrimary().getBounds().getWidth();
 
+
+
+
+
     protected void update(double dt) {
         map.setPrefWidth(((Stage) this.getScene().getWindow()).getWidth());
         headsUpDisplay.setPrefWidth(((Stage) this.getScene().getWindow()).getWidth());
         map.update(dt);
     }
+    public Level(){
 
-    public Level() {
-        this.getChildren().addAll(headsUpDisplay);
-        headsUpDisplay.setAlignment(Pos.CENTER);
-        headsUpDisplay.setStyle("-fx-border-color: black");
-        headsUpDisplay.setMaxHeight(200);
-        headsUpDisplay.setPrefHeight(200);
-        headsUpDisplay.setFillHeight(false);
-        headsUpDisplay.setStyle("-fx-background-color: lightblue; -fx-border-color: black");
-        AnchorPane.setBottomAnchor(headsUpDisplay, 5.0);
     }
+    public Level(Mapp map, String type) throws IOException {
 
-    public Level(Mapp map, String type) {
+        container = BattleGround.gameLoader().load();
+        this.getChildren().add(container);
+        headsUpDisplay = (Pane) (container.getChildren().get(0));
         this.type = type;
-
         if (this.type.equalsIgnoreCase("pve")) {
             map.loot = new Loot(screenWidth - 341, 410);
             map.getChildren().add(map.loot);
         }
-
         if (this.type.equalsIgnoreCase("pvp")) {
             //TODO
         }
-
         this.map = map;
-        this.getChildren().addAll(this.map, headsUpDisplay);
+        container.getChildren().add(this.map);
+        map.toBack();
         this.getChildren().add(trajectoryLine);// TODO arrow
         headsUpDisplay.setMaxHeight(200);
         headsUpDisplay.setPrefHeight(200);
-        headsUpDisplay.setFillHeight(false);
         headsUpDisplay.setMaxWidth(screenWidth);
-        headsUpDisplay.setStyle("-fx-background-color: white; -fx-border-color: black");
-        AnchorPane.setBottomAnchor(headsUpDisplay, 5.0);
     }
 
     protected void displayLoadout(Fighter selectedFighter) {
         //TODO
     }
-
 }
