1f5baa9fd131972f28f0509cec89ee09db342b59
2246982 <123512815+2246982@users.noreply.github.com>
2024-04-03 19:42:51 -0400

    small update for grenades

--- BulletBattleGround/src/main/java/com/example/bulletbattleground/gameObjects/projectiles/Grenade.java
+++ BulletBattleGround/src/main/java/com/example/bulletbattleground/gameObjects/projectiles/Grenade.java
@@ -17,18 +17,18 @@ import lombok.Setter;
 
 public class Grenade extends Projectile {
 
-    Image explosionEffect1 = new Image("file:1.png");
-    Image explosionEffect2 = new Image("file:2.png");
-    Image explosionEffect3 = new Image("file:3.png");
-    Image explosionEffect4 = new Image("file:4.png");
-    Image explosionEffect5 = new Image("file:5.png");
-    Image explosionEffect6 = new Image("file:6.png");
-    Image explosionEffect7 = new Image("file:7.png");
-    Image explosionEffect8 = new Image("file:8.png");
+    Image explosionEffect1 = new Image("file:1.PNG");
+    Image explosionEffect2 = new Image("file:2.PNG");
+    Image explosionEffect3 = new Image("file:3.PNG");
+    Image explosionEffect4 = new Image("file:4.PNG");
+    Image explosionEffect5 = new Image("file:5.PNG");
+    Image explosionEffect6 = new Image("file:6.PNG");
+    Image explosionEffect7 = new Image("file:7.PNG");
+    Image explosionEffect8 = new Image("file:8.PNG");
 
     Circle grenade = new Circle(10, Color.PALEGREEN);
 
-    Circle explosionCircle = new Circle(10, Color.PALEGREEN);
+    Circle explosionCircle = new Circle(20, Color.PALEGREEN);
 
     public Grenade() {
         fuseTimer = 25.0;
@@ -53,29 +53,29 @@ public class Grenade extends Projectile {
         //TODO
     }
 
-
-
     @Override
     public void move(double time) {
         super.move(time);
         fuseTimer = fuseTimer - time;
         explode();
+
     }
 
-    public void explode(){
+    public void explode() {
         if (fuseTimer <= 0.0) {
-            grenade.setRadius(0);
-          /*  Timeline explosionAnimation = new Timeline(new KeyFrame(Duration.millis(1), e
-                    -> {
-                explosionCircle.setFill(new ImagePattern(explosionEffect1));
-
-            }));
-            explosionAnimation.setCycleCount(Timeline.INDEFINITE);
-            explosionAnimation.play();
-*/
-                //TODO set the picture of the explosion here
+            explosionCircle.setLayoutX(getChildren().get(0).getLayoutX());
+            explosionCircle.setLayoutY(getChildren().get(0).getLayoutY());
+            this.getChildren().remove(0);
+            this.getChildren().add(explosionCircle);
+            explosionAnimation();
         }
     }
+
+    public void explosionAnimation() {
+        //TODO create the animation for the explosion with the pictures
+        
+    }
+
 
     @Override
     public String toString() {
