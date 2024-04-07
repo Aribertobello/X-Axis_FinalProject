b12a1085287a6692339f93eb6bdf3e81e1d47b4f
2246982 <123512815+2246982@users.noreply.github.com>
2024-04-07 19:25:14 -0400

    Update MainMenuController.java

--- BulletBattleGround/src/main/java/com/example/bulletbattleground/controllers/MainMenuController.java
+++ BulletBattleGround/src/main/java/com/example/bulletbattleground/controllers/MainMenuController.java
@@ -56,7 +56,7 @@ public class MainMenuController {
     public void player_environ_btn_clicked(ActionEvent event){
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         try{
-            BattleGround.activeGame = new Game(FileManager.defaultLevelPvp());
+            BattleGround.activeGame = new Game(FileManager.defaultLevelPve());
         }catch(IOException e){
          e.printStackTrace();
         }
@@ -72,7 +72,7 @@ public class MainMenuController {
     public void player_player_btn_clicked(ActionEvent event){
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         try{
-            BattleGround.activeGame = new Game(FileManager.defaultLevelPve());
+            BattleGround.activeGame = new Game(FileManager.defaultLevelPvp());
         }catch(IOException e){
             e.printStackTrace();
         }
